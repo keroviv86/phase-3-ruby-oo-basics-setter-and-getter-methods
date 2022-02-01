8219380e9d9bd31541d086a3a145dfaf191fa24f
Vivian Yim <keroviv86@gmail.com>
2022-01-31 21:44:23 -0600

    done

--- lib/person.rb
+++ lib/person.rb
@@ -1,3 +1,21 @@
 class Person
+    def name=(names)
+        @name=names
+    end 
 
+    def name
+        @name
+    end
+
+
+    def job=(job)
+        @job=job
+    end
+
+    def job
+        @job
+    end
 end
+new= Person.new
+new.name="Vivian"
+puts new.name
