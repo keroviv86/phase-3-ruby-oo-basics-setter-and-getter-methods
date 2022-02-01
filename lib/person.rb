348705b6e4fbb32f18ee6fdca87f6e8f06ee7795
Vivian Yim <keroviv86@gmail.com>
2022-01-31 21:43:03 -0600

    commit tree fc2b7ee3c0453c704a3188e1dfaf180e4022f498

--- lib/person.rb
+++ lib/person.rb
@@ -1,3 +1,18 @@
 class Person
+    def name=(names)
+        @name=names
+    end 
 
+    def name
+        @name
+    end
+
+
+    def job=(job)
+        @job=job
+    end
+
+    def job
+        @job
+    end
 end
